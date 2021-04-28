901cef4969f0f6e54edad1a0d0404146591ca1d2
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-04-28 09:50:11 +0300

    Setting up postgres as default db and adding .gitignore
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

--- cashflowsite/cashflowsite/settings.py
+++ cashflowsite/cashflowsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'cashflowsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_USER_PASSWORD'),
+        'HOST': config('DB_HOST')
     }
 }
-
+print(config('DB_USER_PASSWORD'))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
