b45049764f8ecfcd90d788b172d266ad4a8b72a8
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-04-30 10:36:28 +0300

    Adds username validation
    Adding Username validation to the register page.
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

--- cashflowsite/authentication/urls.py
+++ cashflowsite/authentication/urls.py
@@ -1,6 +1,8 @@
 from .views import RegistrationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = {
-    path('register', RegistrationView.as_view(), name="register")
+    path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', UsernameValidationView.as_view(), name="validate-username")
 }
--- cashflowsite/authentication/views.py
+++ cashflowsite/authentication/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error' : 'username should only contain alphanumeric characters'})
+        return JsonResponse({'username_valid', True})
