2b04794e196955ecba682d3d70f9319998364eac
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-05-03 11:56:01 +0300

    Adds show/hide password toggle
    Implements show/hide toggle using setAttribute and textContent methods
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

--- cashflowsite/cashflowsite/static/js/register.js
+++ cashflowsite/cashflowsite/static/js/register.js
@@ -4,6 +4,8 @@ const emailField=document.querySelector("#emailField");
 const emailFeedbackArea=document.querySelector(".emailFeedbackArea");
 const usernameSuccessOutput=document.querySelector(".usernameSuccessOutput");
 const emailSuccessOutput=document.querySelector(".emailSuccessOutput");
+const showPasswordToggle=document.querySelector(".showPasswordToggle");
+const passwordField=document.querySelector("#passwordField");
 
 usernameField.addEventListener("keyup", (e) => {
     const usernameVal=e.target.value;
@@ -54,3 +56,15 @@ emailField.addEventListener("keyup", (e) => {
         });
     }
 });
+
+const handleToggleInput= (e) => {
+    if(showPasswordToggle.textContent==="SHOW"){
+        showPasswordToggle.textContent="HIDE";
+        passwordField.setAttribute("type", "text");
+    }else{
+        showPasswordToggle.textContent="SHOW";
+        passwordField.setAttribute("type", "password");
+    }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
